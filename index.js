3a42999a9afd71184a6b65132d6bf886122bf0be
S-Rabi <102955157+S-Rabi@users.noreply.github.com>
2024-03-21 04:44:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+const port = 9000;
+app.use("/", (req, res) => {
+  res.json({ message: "Hello from express!" });
+});
+app.listen(port, () => {
+  console.log(`Starting server on port ${port}`);
+});
